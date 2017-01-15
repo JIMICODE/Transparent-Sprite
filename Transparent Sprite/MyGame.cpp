6ab879c49cca7f36545b016e2a12affa59f4a134
JIMICODE <1159081156@qq.com>
2017-01-15 17:32:18 +0800

    dynamic buckground scroll

--- Transparent Sprite/MyGame.cpp
+++ Transparent Sprite/MyGame.cpp
@@ -1,15 +1,15 @@
 #include "MyDirectX.h"
 #include<sstream>
-const string APPTITLE = "Tile_Based Static Scrolling";
-const int SCREENW = 1440;
-const int SCREENH = 900;
+const string APPTITLE = "Tile_Based Dynamic Scrolling";
+const int SCREENW = 1024;
+const int SCREENH = 768;
 
 LPD3DXFONT font;
 //setting for the scroller
 const int TILEWIDTH = 64;
 const int TILEHEIGHT = 64;
-const int MAPWIDTH = 25;
-const int MAPHEIGHT = 18;
+const int MAPWIDTH = 16;
+const int MAPHEIGHT = 24;
 //scrolling window size
 const int WINDOWWIDTH = (SCREENW / TILEWIDTH) * TILEWIDTH;
 const int WINDOWHEIGHT = (SCREENH / TILEHEIGHT) * TILEHEIGHT;
@@ -19,28 +19,66 @@ const int GAMEWORLDHEIGHT = TILEHEIGHT * MAPWIDTH;
 int ScrollX, ScrollY;
 int SpeedX, SpeedY;
 long start;
-LPDIRECT3DSURFACE9 gameworld = NULL;
+
+LPDIRECT3DSURFACE9 scrollbuffer = NULL;
+LPDIRECT3DSURFACE9 tiles = NULL;
 int MAPDATA[MAPWIDTH*MAPHEIGHT] = {
-	80,81,81,81,81,81,81,81,81,81,81,81,81,81,81,81,81,81,81,81,81,
-	81,81,81,82,90,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,92,3,3,3,3,3,92,3,
-	92,90,3,13,83,96,3,3,23,3,92,3,13,92,3,3,3,3,3,3,11,3,13,3,3,92,
-	90,3,3,3,3,3,3,3,10,3,3,3,3,3,23,3,3,3,3,3,3,3,13,3,92,90,3,96,
-	3,13,3,3,3,3,3,3,3,3,3,3,3,3,96,3,23,3,96,3,3,92,90,3,3,3,3,3,3,
-	13,3,3,3,13,3,3,11,3,3,3,3,3,3,3,13,3,92,90,3,83,11,3,92,3,3,3,
-	3,3,11,3,3,3,3,3,3,3,83,3,3,3,92,92,90,3,3,3,96,3,13,3,3,3,11,
-	10,3,3,3,3,3,13,3,3,13,3,3,3,92,90,3,23,3,3,3,3,3,3,96,3,3,83,
-	3,3,3,92,3,3,3,3,3,13,3,92,90,3,3,3,3,3,3,3,3,3,3,3,3,23,3,3,3,
-	3,3,3,3,3,3,3,92,90,3,3,3,11,3,92,3,3,13,3,3,131,3,10,3,3,3,96,
-	3,92,3,96,3,92,90,3,13,83,3,3,3,3,3,3,3,3,3,3,3,13,3,3,3,3,3,3,
-	3,3,92,90,3,3,3,3,13,3,3,3,3,3,11,96,3,3,3,3,3,3,13,3,13,3,11,
-	92,90,92,3,13,3,3,3,3,3,3,92,3,10,3,23,3,3,3,3,3,3,3,3,3,92,90,
-	3,3,3,3,3,96,3,23,3,3,3,3,3,3,3,3,83,3,3,13,3,96,3,92,90,3,3,3,
-	3,92,3,3,3,3,3,13,3,3,3,13,3,3,3,11,3,3,3,3,92,90,3,13,3,3,3,3,
-	3,3,3,96,3,3,3,3,3,3,3,3,3,3,92,3,3,92,100,101,101,101,101,101,
-	101,101,101,101,101,101,101,101,101,101,101,101,101,101,101,101,
-	101,101,102 };
+	1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,
+	26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,
+	48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,
+	70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,
+	92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,
+	110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,
+	126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,
+	142,143,144,145,146,147,148,149,150,151,152,153,154,155,156,157,
+	158,159,160,161,162,163,164,165,166,167,168,169,170,171,172,173,
+	174,175,176,177,178,179,180,181,182,183,184,185,186,187,188,189,
+	190,191,192,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
+	21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,
+	42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,
+	63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,
+	84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,
+	104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,
+	120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,
+	136,137,138,139,140,141,142,143,144,145,146,147,148,149,150,151,
+	152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,
+	168,169,170,171,172,173,174,175,176,177,178,179,180,181,182,183,
+	184,185,186,187,188,189,190,191,192 };
 
+//this function updates the scrolling position and speed
+void UpdateScrollPosition()
+{
+	const int GAMEWORLDWIDTH = TILEWIDTH * MAPWIDTH;
+	const int GAMEWORLDHEIGHT = TILEHEIGHT * MAPHEIGHT;
+	//update horizontal scrolling position and speed
+	//update horizontal scrolling position and speed
+	ScrollX += SpeedX;
+	if (ScrollX < 0)
+	{
+		ScrollX = 0;
+		SpeedX = 0;
+	}
+	else if (ScrollX > GAMEWORLDWIDTH - WINDOWWIDTH)
+	{
+		ScrollX = GAMEWORLDWIDTH - WINDOWWIDTH;
+		SpeedX = 0;
+	}
+	//update vertical scrolling position and speed
+	ScrollY += SpeedY;
+	if (ScrollY < 0)
+	{
+		ScrollY = 0;
+		SpeedY = 0;
+	}
+	else if (ScrollY > GAMEWORLDHEIGHT - WINDOWHEIGHT)
+	{
+		ScrollY = GAMEWORLDHEIGHT - WINDOWHEIGHT;
+		SpeedY = 0;
+	}
+}
 
+//this function does the real worl of drawing a single tile from the
+//source image onto the tile scroll buffer
 void DrawTile(
 	LPDIRECT3DSURFACE9 source,
 	int tilenum,
@@ -61,79 +99,66 @@ void DrawTile(
 
 	//set destination rect
 	RECT r2 = { destx, desty, destx + width, desty + height };
-
 	//draw the tile
 	d3ddev->StretchRect(source, &r1, dest, &r2, D3DTEXF_NONE);
 }
 
-void BuildGameWorld()
+//This function files the tilebuffer with tiles representing
+//the current scroll display based on scrollx/scrolly
+void DrawTiles()
 {
-	HRESULT result;
+	int tilex, tiley;
+	int columns, rows;
 	int x, y;
-	LPDIRECT3DSURFACE9 tiles;
+	int tilenum;
 
-	//load the bitmap image containing all the tiles
-	tiles = LoadSurface("groundtiles.bmp");
+	//calculate starting tile position
+	tilex = ScrollX / TILEWIDTH;
+	tiley = ScrollY / TILEHEIGHT;
 
-	//create the scrolling game world bitmap
-	result = d3ddev->CreateOffscreenPlainSurface(
-		GAMEWORLDWIDTH,
-		GAMEWORLDHEIGHT,
-		D3DFMT_X8R8G8B8,
-		D3DPOOL_DEFAULT,
-		&gameworld,
-		NULL
-	);
-	if (result != D3D_OK)
-	{
-		MessageBox(NULL, "Error creating working surface!", "Error", NULL);
-		return;
-	}
-	//filll the game world bitmao with tiles
-	for(y = 0; y < MAPHEIGHT; ++y)
-		for (x = 0; x < MAPWIDTH; ++x)
+	//calculate the number of columns and rows 
+	columns = WINDOWWIDTH / TILEWIDTH;
+	rows = WINDOWHEIGHT / TILEHEIGHT;
+
+	//draw tiles onto the scroll buffer surface
+	for(y = 0; y <= rows; ++y)
+		for (x = 0; x <= columns; ++x)
 		{
-			DrawTile(tiles, MAPDATA[y * MAPWIDTH + x], 64, 64, 16,
-				gameworld, x * 64, y * 64);
+			//retrieve the tile number from this posision
+			tilenum = MAPDATA[((tiley + y) * MAPWIDTH + (tilex + x))];
+			//draw the tile onto the scroll buffer
+			DrawTile(tiles, tilenum, TILEWIDTH, TILEHEIGHT, 16, scrollbuffer,
+				x*TILEWIDTH, y*TILEHEIGHT);
 		}
-
-	//now tiles bitmap is no longer needed
-	tiles->Release();
 }
 
-void ScrollScreen()
+//This function draws the portion of the scroll buffer onto the back
+//buffer according to the current "partial tile" scroll posiotion.
+void DrawScrollWindow(bool scaled = false)
 {
-	//update horizontal scrolling position and speed
-	ScrollX += SpeedX;
-	if (ScrollX < 0)
-	{
-		ScrollX = 0;
-		SpeedX = 0;
-	}
-	else if (ScrollX > GAMEWORLDWIDTH - SCREENW)
-	{
-		ScrollX = GAMEWORLDWIDTH - SCREENW;
-		SpeedX = 0;
-	}
-	//update vertical scrolling position and speed
-	ScrollY += SpeedY;
-	if (ScrollY < 0)
-	{
-		ScrollY = 0;
-		SpeedY = 0;
-	}
-	else if (ScrollY > GAMEWORLDHEIGHT - SCREENH)
-	{
-		ScrollY = GAMEWORLDHEIGHT - SCREENH;
-		SpeedY = 0;
-	}
-	//set dimensions of the source image
-	RECT r1 = { ScrollX, ScrollY, ScrollX + SCREENW - 1, ScrollY + SCREENH - 1 };
-	//set the destination rect
-	RECT r2 = { 0,0,SCREENW - 1, SCREENH - 1 };
+	//calculate the partial sub-tile lines to draw using modulus
+	int partialx = ScrollX % TILEWIDTH;
+	int partialy = ScrollY % TILEHEIGHT;
 
-	//set the current game world view
-	d3ddev->StretchRect(gameworld, &r1, backbuffer, &r2, D3DTEXF_NONE);
+	//set dimensions of the source image as a rectangle
+	RECT r1 = { partialx, partialy, partialx + WINDOWWIDTH - 1, partialy + WINDOWHEIGHT - 1 };
+	//set the destination rectangle
+	RECT r2;
+	if (scaled)
+	{
+		//used this line for scaled display
+		RECT r = { 0, 0, WINDOWWIDTH - 1, WINDOWHEIGHT - 1 };
+		r2 = r;
+	}
+	else
+	{
+		//used this line foe none-scalede display
+		RECT r = { 0, 0, SCREENW - 1, SCREENH - 1 };
+		r2 = r;
+	}
+
+	//draw the "partial tile" scroll window onto the backbuffer
+	d3ddev->StretchRect(scrollbuffer, &r1, backbuffer, &r2, D3DTEXF_NONE);
 }
 
 bool Game_Init(HWND window)
@@ -154,7 +179,20 @@ bool Game_Init(HWND window)
 	d3ddev->GetBackBuffer(0, 0, D3DBACKBUFFER_TYPE_MONO, &backbuffer);
 	//create a font
 	font = MakeFont("Arial", 24);
-	BuildGameWorld();
+	//load the tile image
+	tiles = LoadSurface("spacemap.bmp");
+	if (!tiles) return false;
+	//create the scroll buffer surface in memory, slightly bigger than the screen
+	const int SCROLLBUFFERWIDTH = SCREENW + TILEWIDTH * 2;
+	const int SCROLLBUFFERHEIGHT = SCREENH + TILEHEIGHT + 2;
+	HRESULT result = d3ddev->CreateOffscreenPlainSurface(
+		SCROLLBUFFERWIDTH, SCROLLBUFFERHEIGHT,
+		D3DFMT_X8R8G8B8, D3DPOOL_DEFAULT,
+		&scrollbuffer,
+		NULL
+	);
+	if (result != D3D_OK)	return false;
+
 	start = GetTickCount();
 
 
@@ -188,8 +226,10 @@ void Game_Run(HWND window)
 		//start rendering
 		if (d3ddev->BeginScene())
 		{
-			//update the scrolling view
-			ScrollScreen();
+			//draw tiles onto the scroll buffer
+			DrawTiles();
+			//draw the scroll window onto the back buffer
+			DrawScrollWindow();
 			spriteobj->Begin(D3DXSPRITE_ALPHABLEND);
 			std::ostringstream oss;
 			oss << "Scroll Posiotion = " << ScrollX << "," << ScrollY;
@@ -225,7 +265,8 @@ void Game_Run(HWND window)
 
 void Game_End()
 {
-	if (gameworld) gameworld->Release();
+	if (scrollbuffer)	scrollbuffer->Release();
+	if (tiles)	tiles->Release();
 	DirectInput_Shutdown();
 	Direct3D_ShutDown();
 }
